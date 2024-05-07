7723ec04dd31a97248adf5a1a0dd26660a1c3a0f
Berend <berendgorths@gmail.com>
2024-05-07 14:11:03 +0200

    pushing github

--- run_scripts.sh
+++ run_scripts.sh
@@ -1,7 +1,7 @@
 
 
 # mini ds (21 traces)
-python src/main.py --output_dir experiments --comment "unsupervised pretraining" --name container_traces_pretrained --records_file Unsupervised_records.xls --data_dir src/container_traces_21_a_b_c_d_e_f.csv --data_class container_traces --pattern TRAIN --val_ratio 0.2 --epochs 700 --lr 0.001 --optimizer RAdam --batch_size 8 --pos_encoding learnable --d_model 64 --dim_feedforward 256 --num_layers 1 --num_heads 1
+python src/main.py --output_dir experiments --comment "unsupervised pretraining" --name container_traces_pretrained --records_file Unsupervised_records.xls --data_dir src/container_traces_21_a_b_c_d_e_f.csv --data_class container_traces --pattern TRAIN --val_ratio 0.2 --epochs 700 --lr 0.001 --optimizer RAdam --batch_size 8 --pos_encoding learnable --d_model 32 --dim_feedforward 64 --num_layers 1 --num_heads 1
 
 
 # Saving for feature reference
